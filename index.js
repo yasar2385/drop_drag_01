265096cbb04f643aa72f777103c4056f91d406d3
yasar2385 <yasar.mohideen@newgen.co>
2021-08-27 10:24:34 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 // Import stylesheets
 import './style.css';
+// Default SortableJS
+//import Sortable from 'sortablejs';
+
+// Core SortableJS (without default plugins)
+import { AutoScroll } from 'sortablejs/modular/sortable.core.esm.js';
+
+// Complete SortableJS (with all plugins)
+import Sortable from 'sortablejs/modular/sortable.complete.esm.js';
+// Cherrypick extra plugins
+// import Sortable, { MultiDrag, Swap, AutoScroll } from 'sortablejs';
+
+// Sortable.mount(new MultiDrag(), new Swap());
+
+// Cherrypick default plugins
+// import Sortable, { AutoScroll } from 'sortablejs/modular/sortable.core.esm.js';
+
+//Sortable.mount(new AutoScroll());
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
@@ -14,8 +31,8 @@ Sortable.create(bar, {
   group: {
     name: 'bar',
     put: 'qux',
-    pull: function (to, from) {
-      console.log(to.el.children, from.el.children)
+    pull: function(to, from) {
+      console.log(to.el.children, from.el.children);
       return from.el.children.length > 2 || 'clone';
     }
   },
@@ -25,8 +42,8 @@ Sortable.create(bar, {
 Sortable.create(qux, {
   group: {
     name: 'qux',
-    put: function (to) {
-      console.log(to.el.children)
+    put: function(to) {
+      console.log(to.el.children);
       return to.el.children.length < 4;
     }
   },
